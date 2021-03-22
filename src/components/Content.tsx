238b33610b53cfbdc788a71ea32110029a7fb4dc
luizu <luizucastilho@gmail.com>
2021-03-21 22:01:14 -0300

    Chore: Content component created

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+
+interface IContentProps {
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Value: string;
+    }>;
+    Runtime: string;
+    imdbID: string;
+  }>;
+  selectedGenreTitle: string;
+};
+
+export function Content({ movies, selectedGenreTitle }: IContentProps) {
+
+ return (
+  <div className="container">
+  <header>
+    <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+  </header>
+
+  <main>
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+</div>
+ )
 }
